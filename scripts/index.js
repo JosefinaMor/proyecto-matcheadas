e3bf2bdd9213561974bae29cdd8c0d71146eec5f
JosefinaMor <josefina.morales.1765@gmail.com>
2021-09-11 15:52:06 -0300

    agrego estilos para empezar con el generador de matriz para el tablero

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,18 @@ const botonGenerarPiezas = document.getElementById("boton-generar-piezas");
 const contenedorPiezasJuego = document.getElementById("contenedor-piezas-juego");
 
 const arrayPiezas = ["fas fa-broom","fas fa-cat","fas fa-crow","fas fa-ghost","fas fa-hat-wizard","fas fa-skull-crossbones","fas fa-book-dead","fas fa-spider"];
-
+//funcionalidad de generar un num al azar para posiciones del array
+const generarNroAlAzar = (array) =>{
+    return Math.floor(Math.random()*array.length);
+}
 //funcionalidad de generar piezas al azar
 const generarPiezaAlAzar = () =>{
     console.log(arrayPiezas[generarNroAlAzar(arrayPiezas)]);
 }
-const generarNroAlAzar = (array) =>{
-    return Math.floor(Math.random()*array.length);
+//funcionalidad de generar tablero
+const generarTablero = () =>{
+    
 }
+
 
 botonGenerarPiezas.onclick = generarPiezaAlAzar;
